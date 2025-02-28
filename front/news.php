281a437e18a77e053ef34f7e20ae932b8275cf44
sandy0v0 <be0324ss@gmail.com>
2025-03-01 04:12:58 +0800

    建立最新文章顯示

--- front/news.php
+++ front/news.php
@@ -1,1 +1,66 @@
-最新文章
+<fieldset>
+    <legend>目前位置：首頁 > 最新文章區</legend>
+
+    <table style="width: 100%;">
+        <tr>
+            <th width="20%">標題</th>
+            <th width="60%">內容</th>
+            <th></th>
+        </tr>
+        <?php
+
+        $total=$News->count();
+        $div=5;
+        $pages=ceil($total/$div);
+        $now=$_GET['p']??1;
+        $start=($now-1)*$div;
+        $rows=$News->all(['sh'=>1]," Limit $start,$div");
+        foreach($rows as $row):
+        ?>
+        <tr>
+            <td class="row-title"><?=$row['title'];?></td>
+            <td>
+                <span class='title'><?=mb_substr($row['news'],0,25);?>...</span>
+                <span class='detail'><?=nl2br($row['news']);?></span>
+            </td>
+            <td></td>
+        </tr>
+        <?php endforeach; ?>
+    </table>
+    <div>
+        <?php 
+            if(($now-1)>0){
+                echo "<a href='?do=news&p=".($now-1)."'> &lt;</a>";
+            }
+            for($i=1;$i<=$pages;$i++){
+                $size=($i==$now)?"24px":"16px";
+                echo "<a href='?do=news&p=$i' style='font-size:$size'> $i </a>";
+            }
+            if(($now+1)<=$pages){
+                echo "<a href='?do=news&p=".($now+1)."'> &gt;</a>";
+            }
+?>
+    </div>
+</fieldset>
+<script>
+    $(".like").on("click",function(){
+        let id=$(this).data('id');
+        let like=$(this).text();
+
+        $.post("./api/like.php",{id},()=>{
+
+            switch(like){
+                case "讚":
+                    $(this).text("收回讚");
+                break;
+                case "收回讚":
+                    $(this).text("讚");
+                break;
+            }
+        })
+    })    
+    
+$(".row-title").on("click",function(){
+    $(this).next().children(".title,.detail").toggle();
+})
+</script>
