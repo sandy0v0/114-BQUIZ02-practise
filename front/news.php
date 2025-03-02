a46518551f608cde36fd06631a01d8e45dc33949
sandy0v0 <be0324ss@gamil.com>
2025-03-02 13:07:21 +0800

    建立按讚的js基礎程式碼

--- front/news.php
+++ front/news.php
@@ -9,36 +9,58 @@
         </tr>
         <?php
 
-        $total=$News->count();
-        $div=5;
-        $pages=ceil($total/$div);
-        $now=$_GET['p']??1;
-        $start=($now-1)*$div;
-        $rows=$News->all(['sh'=>1]," Limit $start,$div");
-        foreach($rows as $row):
+        $total = $News->count();
+        $div = 5;
+        $pages = ceil($total / $div);
+        $now = $_GET['p'] ?? 1;
+        $start = ($now - 1) * $div;
+        $rows = $News->all(['sh' => 1], " Limit $start,$div");
+        foreach ($rows as $row):
         ?>
         <tr>
-            <td class="row-title"><?=$row['title'];?></td>
+            <td class="row-title"><?= $row['title']; ?></td>
             <td>
-                <span class='title'><?=mb_substr($row['news'],0,25);?>...</span>
-                
+                <span class='title'><?= mb_substr($row['news'], 0, 25); ?>...</span>
+                <span class='detail'><?= nl2br($row['news']); ?></span>
             </td>
-            <td></td>
+            <td>
+                <?php 
+                    if(isset($_SESSION['user'])){
+                        echo "<a href='#' data-id='{$row['id']}' class='like'>讚</a>";
+                    }
+    
+                    ?>
+            </td>
         </tr>
         <?php endforeach; ?>
     </table>
     <div>
-        <?php 
-            if(($now-1)>0){
-                echo "<a href='?do=news&p=".($now-1)."'> &lt;</a>";
-            }
-            for($i=1;$i<=$pages;$i++){
-                $size=($i==$now)?"24px":"16px";
-                echo "<a href='?do=news&p=$i' style='font-size:$size'> $i </a>";
-            }
-            if(($now+1)<=$pages){
-                echo "<a href='?do=news&p=".($now+1)."'> &gt;</a>";
-            }
-?>
+        <?php
+        if (($now - 1) > 0) {
+            echo "<a href='?do=news&p=" . ($now - 1) . "'> &lt;</a>";
+        }
+        for ($i = 1; $i <= $pages; $i++) {
+            $size = ($i == $now) ? "24px" : "16px";
+            echo "<a href='?do=news&p=$i' style='font-size:$size'> $i </a>";
+        }
+        if (($now + 1) <= $pages) {
+            echo "<a href='?do=news&p=" . ($now + 1) . "'> &gt;</a>";
+        }
+        ?>
     </div>
 </fieldset>
+<script>
+$(".like").on("click", function() {
+    let id = $(this).data('id');
+    let like = $(this).text();
+
+    switch (like) {
+        case "讚":
+            $(this).text("收回讚");
+            break;
+        case "收回讚":
+            $(this).text("讚");
+            break;
+    }
+})
+</script>
